83e0e131229f60128d43a69d61f945b4ff65188e
Fadhel <lassouedfad@gmail.com>
2025-08-01 19:06:36 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Fadhel Lassoued on 2025-08-01.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
